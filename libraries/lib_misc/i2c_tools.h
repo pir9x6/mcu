88ad14c383c058e9bdd7bcd52ee6dfbe69d41edc
pir9x6 <pierre.blache.54@gmail.com>
2020-05-01 16:40:30 +0200

    updated i2c_tools (new style)

--- libraries/lib_misc/i2c_tools.h
+++ libraries/lib_misc/i2c_tools.h
@@ -1,5 +1,5 @@
-#ifndef _I2C_TOOLS_H_
-#define _I2C_TOOLS_H_
+#ifndef LIB_MISC_I2C_TOOLS_H
+#define LIB_MISC_I2C_TOOLS_H
 
 #include "delays.h"
 #include "i2c.h"
@@ -7,8 +7,8 @@
 #include "types.h"
 #include "uart.h"
 
-RESULT i2c_detect(UART_ID uart_id, I2C_BUS i2c_id);
+result_t i2c_detect(UART_ID uart_id, I2C_BUS i2c_id);
 
-RESULT i2c_dump(UART_ID uart_id, I2C_BUS i2c_id, u8 adr_chip);
+result_t i2c_dump(UART_ID uart_id, I2C_BUS i2c_id, u8 adr_chip);
 
 #endif

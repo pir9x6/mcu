8379cf79247af0f7e618ec7bbda4b0bf8f872c48
pir9x6 <pierre.blache.54@gmail.com>
2020-05-01 16:39:55 +0200

    moved 'BIT' definitions in types.h

--- libraries/lib_misc/types.h
+++ libraries/lib_misc/types.h
@@ -76,4 +76,22 @@ typedef enum
 #define ROM     const
 #endif
 
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+#define BIT_0    0x0001
+#define BIT_1    0x0002
+#define BIT_2    0x0004
+#define BIT_3    0x0008
+#define BIT_4    0x0010
+#define BIT_5    0x0020
+#define BIT_6    0x0040
+#define BIT_7    0x0080
+#define BIT_8    0x0100
+#define BIT_9    0x0200
+#define BIT_10   0x0400
+#define BIT_11   0x0800
+#define BIT_12   0x1000
+#define BIT_13   0x2000
+#define BIT_14   0x4000
+#define BIT_15   0x8000
+
 #endif

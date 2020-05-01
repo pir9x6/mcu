cd721986f50e033d9b04204c730bb061628290e7
pir9x6 <pierre.blache.54@gmail.com>
2020-05-01 16:39:30 +0200

    Updated 'TMP75' library

--- libraries/lib_comp/tmp75.c
+++ libraries/lib_comp/tmp75.c
@@ -1,72 +1,64 @@
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-//&&&   Title           :   Temperature Sensor TMP75                        &&&
-//&&&   Autor           :   Pierre Blaché                                   &&&
-//&&&   Date            :   May 2014                                        &&&
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-//&&&   Version         :                                                   &&&
-//&&&       v1.0 - 14/05/25 - Creation                                      &&&
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& 
-
 #include "tmp75.h"
-
+#include "types.h"
+#include "i2c.h"
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-//-------------------------- Configure sensor ---------------------------------
+//------------------------ Sensor configuration -------------------------------
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-RESULT tmp75_configure(I2C_BUS bus_id, u8 chip_addr)
+result_t tmp75_configure(I2C_BUS bus_id, u8 dev_addr)
 {
-    u8 tmp;
-    u8 error = 0;
-    RESULT result = SUCCESS;
-
     // resolutions 12 bits, 220 ms for one conversion
-    result = i2c_write_reg(bus_id, chip_addr, TMP75_REG_CONFIGURATION, 0x60);
-    if (result != SUCCESS) error++;
+    if (i2c_write_reg(bus_id, dev_addr, TMP75_REG_CONFIGURATION, 0x60) != SUCCESS)
+        return ERROR;
 
     // low limit
-    result = i2c_write_reg(bus_id, chip_addr, TMP75_REG_LIMIT_LOW, 0x00);
-    if (result != SUCCESS) error++;
+    if (i2c_write_reg(bus_id, dev_addr, TMP75_REG_LIMIT_LOW, 0x00) != SUCCESS)
+        return ERROR;
 
     // high limit
-    result = i2c_write_reg(bus_id, chip_addr, TMP75_REG_LIMIT_HIGH, 0x30);
-    if (result != SUCCESS) error++;
-
-    // check if the chip has been well configured
-    result = i2c_read_reg(bus_id, chip_addr, TMP75_REG_CONFIGURATION, &tmp);
-    if (tmp != 0x60 || result != SUCCESS) error++;
-
-    result = i2c_read_reg(bus_id, chip_addr, TMP75_REG_LIMIT_LOW, &tmp);
-    if (tmp != 0x00 || result != SUCCESS) error++;
-
-    result = i2c_read_reg(bus_id, chip_addr, TMP75_REG_LIMIT_HIGH, &tmp);
-    if (tmp != 0x30 || result != SUCCESS) error++;
-
-
-    if (error != 0)
+    if (i2c_write_reg(bus_id, dev_addr, TMP75_REG_LIMIT_HIGH, 0x30) != SUCCESS)
         return ERROR;
-    else
-        return SUCCESS;
+
+    return SUCCESS;
 }
 
 
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-//----------------- Lecture de la Temperature sur le TMP75 -------------------
+//-------------------------- Temperature Reading ------------------------------
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-RESULT tmp75_read_temp(I2C_BUS bus_id, u8 chip_addr, u16 *temp)
+result_t tmp75_read_temp(I2C_BUS bus_id, u8 dev_addr, u16 *temp)
 {
     u16 tmp = 0;
     u8 i2cbyte1 = 0;
     u8 i2cbyte2 = 0;
 
-    i2c_start (bus_id);
-    i2c_write (bus_id, (chip_addr<<1) & 0xFE);      // address of the chip
-    i2c_write (bus_id, TMP75_REG_TEMPERATURE);      // address of the register
-    i2c_rstart (bus_id);
-    i2c_write (bus_id, (chip_addr<<1) | 0x01);      // next operation is a reading
-    i2c_read (bus_id, &i2cbyte1, I2C_ACK);          // integer part of the temperature
-    i2c_read (bus_id, &i2cbyte2, I2C_NACK);         // decimal part of the temperature
-    i2c_stop (bus_id);
+    if (i2c_start (bus_id) != SUCCESS)
+        return ERROR;
+
+    if (i2c_write (bus_id, (dev_addr<<1) & 0xFE) != SUCCESS)
+        return ERROR;
+
+    if (i2c_write (bus_id, TMP75_REG_TEMPERATURE) != SUCCESS)
+        return ERROR;
+
+    if (i2c_rstart (bus_id) != SUCCESS)
+        return ERROR;
+
+    /* next operation is a reading */
+    if (i2c_write (bus_id, (dev_addr<<1) | 0x01) != SUCCESS)
+        return ERROR;
+
+    /* get integer part of the temperature */
+    if (i2c_read (bus_id, &i2cbyte1, I2C_ACK) != SUCCESS)
+        return ERROR;
+
+    /* get decimal part of the temperature */
+    if (i2c_read (bus_id, &i2cbyte2, I2C_NACK) != SUCCESS)
+        return ERROR;
+
+    if (i2c_stop (bus_id) != SUCCESS)
+        return ERROR;
 
     tmp = (s16) (i2cbyte1<<8);
     tmp = (tmp | i2cbyte2)>>4;
--- libraries/lib_comp/tmp75.h
+++ libraries/lib_comp/tmp75.h
@@ -1,18 +1,15 @@
-#ifndef _TMP75_H_
-#define _TMP75_H_
+#ifndef LIB_COMP_TMP75_H
+#define LIB_COMP_TMP75_H
 
 #include "types.h"
 #include "i2c.h"
-
-#define TMP75_ADDRESS               0x48
 
 #define TMP75_REG_TEMPERATURE       0x00
 #define TMP75_REG_CONFIGURATION     0x01
 #define TMP75_REG_LIMIT_LOW         0x02
 #define TMP75_REG_LIMIT_HIGH        0x03
 
-RESULT tmp75_read_temp(I2C_BUS bus_id, u8 chip_addr, u16 *temp);
-
-RESULT tmp75_configure(I2C_BUS bus_id, u8 chip_addr);
+result_t tmp75_read_temp(I2C_BUS bus_id, u8 dev_addr, u16 *temp);
+result_t tmp75_configure(I2C_BUS bus_id, u8 dev_addr);
 
 #endif

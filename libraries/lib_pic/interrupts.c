bb4e916ecb7d701527ddfc68cacbe7dc707d1e31
pir9x6 <pierre.blache.54@gmail.com>
2020-05-23 00:58:46 +0200

    added global interrupts config

---
+++ libraries/lib_pic/interrupts.c
@@ -0,0 +1,45 @@
+#include "xc.h"
+
+void enable_global_interrutps(void)
+{
+    /* enable interrupts (global, peripheral, high priority) */
+#if defined (_18F252)
+
+    INTCONbits.GIE = 1;       
+    INTCONbits.PEIE = 1;
+    RCONbits.IPEN = 1; 
+
+#elif defined (_18F26K42)
+
+    INTCON0bits.GIEH = 1;   
+    INTCON0bits.GIEL = 1;   
+    INTCON0bits.IPEN = 1;   
+
+#else
+
+    #error -- processor ID not specified in generic header file
+
+#endif
+}
+
+void disable_global_interrutps(void)
+{
+    /* disnable interrupts (global, peripheral, high priority) */
+#if defined (_18F252)
+
+    INTCONbits.GIE = 0;       
+    INTCONbits.PEIE = 0;
+    RCONbits.IPEN = 0; 
+
+#elif defined (_18F26K42)
+
+    INTCON0bits.GIEH = 0;   
+    INTCON0bits.GIEL = 0;   
+    INTCON0bits.IPEN = 0;   
+
+#else
+
+    #error -- processor ID not specified in generic header file
+
+#endif
+}
---
+++ libraries/lib_pic/interrupts.h
@@ -0,0 +1,10 @@
+#ifndef LIB_PIC_INTERRUPTS_H
+#define LIB_PIC_INTERRUPTS_H
+
+#include "types.h"
+#include <xc.h>
+
+void enable_global_interrutps(void);
+void disable_global_interrutps(void);
+    
+#endif

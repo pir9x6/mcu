f8a0aeea586c1b8a5b29878d37d73585e02e5e5f
pir9x6 <pierre.blache.54@gmail.com>
2020-05-24 18:10:31 +0200

    updated main (light & temp sensors...)

--- projects/pitechoid_pic18_dip28_ref/main.c
+++ projects/pitechoid_pic18_dip28_ref/main.c
@@ -15,43 +15,37 @@
 #pragma warning disable 759     // expression generates no code
 #pragma warning disable 1498    // pointer in expression may have no targets
 #pragma warning disable 1510    // non-reentrant function
+#pragma warning disable 2020
 
 //-------------------------------- includes -----------------------------------
+#include "config.h"
 #include "xc.h"
+#include "stdio.h"
 #include "hardware_profile.h"
 
 #include "bcd.h"
+#include "bh1750.h"
+#include "ccp.h"
 #include "console.h"
 #include "date_time.h"
 #include "delays.h"
 #include "ds1307.h"
+#include "ds1631.h"
 #include "i2c.h"
+#include "interrupts.h"
 #include "io.h"
 #include "lcd_2x16.h"
 #include "log.h"
 #include "pcf8574.h"
+#include "pin_manager.h"
 #include "pwm.h"
 #include "timer.h"
 #include "tmp75.h"
 #include "types.h"
 #include "uart.h"
 
-//--------------------------- Configuration bits ------------------------------
-#pragma config WDT = OFF                // Watchdog OFF
-#pragma config OSC = HS                 // Quartz 10MHz
-#pragma config LVP = OFF                // Low Power Prog OFF
-#pragma config BOR = OFF                // Disable Brown Out
-
 //-------------------------------- Defines ------------------------------------
 #define SEG_OFF         10
-#define LED_ERROR       LATAbits.LATA0      // Error LED
-#define LED_SEC         LATAbits.LATA1
-#define SWITCH_1        PORTAbits.RA2
-#define SWITCH_2        PORTAbits.RA3
-
-#define LED_R           LATBbits.LATB0
-#define LED_G           LATBbits.LATB1
-#define LED_B           LATBbits.LATB2
 
 #define LED_RGB_ON      0
 #define LED_RGB_OFF     1
@@ -66,69 +60,33 @@ bool_t time_has_changed_user = FALSE;
 UART_ID UART_LOG = UART_ID_1;
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-//---------------------------- Timer 2 Interrupt ------------------------------
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-void __interrupt(high_priority) timer2_irq()
-{
-    static u16 CntTmrIncSec = 0;
-
-    T2CONbits.TMR2ON = 0;               // Disable Timer 2
-    PIR1bits.TMR2IF = 0;                // Clear Timer interrupt
-
-    // NOP10; NOP10; NOP10; NOP10; NOP10; NOP10; NOP10; NOP10;
-
-    T2CONbits.TMR2ON = 1;             // Timer 2 on
-    
-    CntTmrIncSec++;
-
-    if (CntTmrIncSec == 499){
-        LED_SEC = !LED_SEC;
-    }
-    else if (CntTmrIncSec == 999)
-    {
-        time_has_changed_timer = TRUE;
-        LED_SEC = !LED_SEC;
-        CntTmrIncSec = 0;
-    }
-}
-
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 //-------------------------------- Main Program -------------------------------
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 void main (void)
 {
     u8 i, j;
     u8 cnt = 0;
+    float light;
     date_time_t t = {
-            t.hrs = 22,
-            t.min = 17,
+            t.hrs =  7,
+            t.min =  0,
             t.sec =  0,
             t.dow =  6,
-            t.day = 19,
-            t.mth = 04,
+            t.day = 24,
+            t.mth = 05,
             t.yrs = 20
     };
     lcd_config_t lcd_config = {
         lcd_config.nb_lines = NB_LINES_2,
         lcd_config.nb_bits = NB_BITS_4
     };
-
-    u8 bcd[5];
     float tmp75_temp;
-    u8 segments[11] = {
-        0x40, 0x79, 0x24, 0x30, 0x19, 0x12, 0x02, 0x78, 0x00, 0x10, 0x3F
-    };   // 0 = On
+    float ds1631_temp;
 
     u8 red_level, green_level, blue_level;
         
 //--------------------------------- GPIO init ---------------------------------
-    set_port_A_input(BIT_3 | BIT_2);
-    set_port_B_input(0);
-    set_port_C_input(BIT_6);
-
-    /* PORTA as digital IOs */
-    ADCON1 = 0x0F;
+    pin_manager_init();
 
     /* default values */
     LED_ERROR = 0;
@@ -139,6 +97,7 @@ void main (void)
         PRINT_ERROR("UART initialization failed");
         LED_ERROR = 1;
     }
+    printf("\nCoucou \n");
     
 //--------------------- i2c, bus & devices initialization ---------------------
     if (i2c_init(I2C_BUS_1, I2C_FREQ, I2C_MASTER) != SUCCESS){
@@ -150,38 +109,73 @@ void main (void)
 
 //-------------------------- Interruption sur Timer 2 -------------------------
     /* Timer period = Fosc(10M) / 4 /  Prescaler(1) / Postscaler(10) / Timer(250) / Cnt(1000) = 1s */
-    if (timer_init(TIMER_ID_2, TMR_PRESCALER_1, TMR_POSTSCALER_10, 249/*timer*/) != SUCCESS){
+    if (timer_init(TIMER_ID_2, TMR_PRESCALER_1, TMR_POSTSCALER_10, 49/*249timer*/) != SUCCESS){
         PRINT_ERROR("Timer initialization failed");
         LED_ERROR = 1;
     }
 
-//----------------------------- PWM Configuration------------------------------
-    if (pwm_init(PWM_ID_1, 0/*freq*/, 255/*duty*/) != SUCCESS){
-        PRINT_ERROR("PWM initialization failed");
-        LED_ERROR = 1;
-    }
+//------------------------ CCP Configuration (PWM mode) -----------------------
+    // if (ccp_init(CCP_ID_1, TIMER_ID_2, 0/*freq*/, 255/*duty*/) != SUCCESS){
+    //     PRINT_ERROR("CCP #1 initialization failed");
+    //     LED_ERROR = 1;
+    // }
+    // ccp_set_pwm_duty(CCP_ID_1, 512);
+
+    // if (ccp_init(CCP_ID_2, TIMER_ID_4, 0/*freq*/, 255/*duty*/) != SUCCESS){
+    //     PRINT_ERROR("CCP #2 initialization failed");
+    //     LED_ERROR = 1;
+    // }
+    // ccp_set_pwm_duty(CCP_ID_2, 512);
+
+    // if (ccp_init(CCP_ID_3, TIMER_ID_2, 0/*freq*/, 255/*duty*/) != SUCCESS){
+    //     PRINT_ERROR("CCP #3 initialization failed");
+    //     LED_ERROR = 1;
+    // }
+    // ccp_set_pwm_duty(CCP_ID_3, 512);
+
+    // if (ccp_init(CCP_ID_4, TIMER_ID_2, 0/*freq*/, 255/*duty*/) != SUCCESS){
+    //     PRINT_ERROR("CCP #4 initialization failed");
+    //     LED_ERROR = 1;
+    // }
+    // ccp_set_pwm_duty(CCP_ID_4, 512);
 
 //--------------------------------- LCD Init ----------------------------------
     lcd_2x16_init(lcd_config);
 
 //------------------------------------ RTC ------------------------------------
-    // if (ds1307_init(I2C_BUS_1, I2C_ADR_DS1307) != SUCCESS){
-    //     PRINT_ERROR("DS1307 initialization failed");
+    if (ds1307_init(I2C_BUS_1, I2C_ADR_DS1307) != SUCCESS){
+        PRINT_ERROR("DS1307 initialization failed");
+        LED_ERROR = 1;
+    }
+    // if (ds1307_set_time(I2C_BUS_1, I2C_ADR_DS1307, t) != SUCCESS){
+    //     PRINT_ERROR("Unable to set RTC date & time");
     //     LED_ERROR = 1;
     // }
-    // delay_ms(100);
-    // ds1307_set_time(I2C_BUS_1, I2C_ADR_DS1307, t);
-    // if (ds1307_get_time(I2C_BUS_1, I2C_ADR_DS1307, &t) != SUCCESS){
-    //     PRINT_ERROR("Unable to get RTC date & time");
-    //     LED_ERROR = 1;
-    // }
+    if (ds1307_get_time(I2C_BUS_1, I2C_ADR_DS1307, &t) != SUCCESS){
+        PRINT_ERROR("Unable to get RTC date & time");
+        LED_ERROR = 1;
+    }
 
-//----------------------------- Temperature Sensor ----------------------------
+//---------------------------------- Sensors ----------------------------------
     if (tmp75_configure(I2C_BUS_1, I2C_ADR_TMP75) != SUCCESS){
         PRINT_ERROR("TMP75 initialization failed");
         LED_ERROR = 1;
     }
 
+    if (bh1750_init(I2C_BUS_1, I2C_ADR_BH1750) != SUCCESS){
+        PRINT_ERROR("BH1750 initialization failed");
+        LED_ERROR = 1;
+    }
+
+    if (ds1631_config(I2C_BUS_1, I2C_ADR_DS1631, DS1631_CFG_RES_12B | DS1631_CFG_CONTINUOUS) != SUCCESS){
+        PRINT_ERROR("DS1631 initialization failed");
+        LED_ERROR = 1;
+    }
+
+//----------------------------- Global Interrupts -----------------------------
+    enable_global_interrutps();
+
+//-----------------------------------------------------------------------------
 #if 0
     while(1){
         LED_R = 0; LED_G = 0; LED_B = 0; delay_ms(1000);
@@ -245,16 +239,29 @@ void main (void)
     }
 #endif
 
+#if 1
     while (1){
-        delay_ms(500);
+        delay_ms(1000);
+
         // uart_write_string(UART_ID_1, "coucou ");
-        pcf8574_write_port(I2C_BUS_1, I2C_ADR_PCF8574A, cnt++);
+        if (pcf8574_write_port(I2C_BUS_1, I2C_ADR_PCF8574A, cnt++) != SUCCESS)
+            LED_ERROR = 1;
 
         lcd_write_time(t, LCD_LINE_1, 1/*position*/);
         lcd_write_date(t, LCD_LINE_2, 1/*position*/, LCD_DATE_LETTERS);
 
-        tmp75_read_temp(I2C_BUS_1, I2C_ADR_TMP75, &tmp75_temp);
+        if (ds1631_read_temp(I2C_BUS_1, I2C_ADR_DS1631, &ds1631_temp) != SUCCESS)
+            LED_ERROR = 1;
+        printf("Temp(DS1631): %2.1f°C   ", ds1631_temp);
+
+        if (tmp75_read_temp(I2C_BUS_1, I2C_ADR_TMP75, &tmp75_temp) != SUCCESS)
+            LED_ERROR = 1;
         lcd_write_temperature(tmp75_temp, LCD_LINE_1, 10/*pos*/);
+        printf("Temp(TMP75): %2.1f°C   ", tmp75_temp);
+
+        if (bh1750_get_light(I2C_BUS_1, I2C_ADR_BH1750, &light) != SUCCESS)
+            LED_ERROR = 1;
+        printf("Light: %2.1f lx\n",  light);
 
         if (time_has_changed_user || time_has_changed_timer){
             if (time_has_changed_user){
@@ -283,5 +290,7 @@ void main (void)
             }
         }
     }
+#endif
+
 }
 

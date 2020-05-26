33d498f300923c57d978be084707fc595032a09f
pir9x6 <pierre.blache.54@gmail.com>
2020-05-26 17:30:44 +0200

    removed useless interrupts functions + use the same function for every interrupts

--- projects/pitechoid_pic18_dip28_ref/interrupts_management.c
+++ projects/pitechoid_pic18_dip28_ref/interrupts_management.c
@@ -1,103 +1,53 @@
+#include "console.h"
 #include "interrupts_management.h"
 #include "hardware_profile.h"
 #include "types.h"
-#include "xc.h"
+#include "pic_compiler.h"
 
 extern bool_t time_has_changed_timer;
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-//--------------------- Timer 1 Interrupt Sub Routine -------------------------
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-void timer1_isr (void)
-{
-
-}
-
-void timer2_isr (void)
-{
-
-}
-
-void timer3_isr (void)
-{
-
-}
-
-void timer4_isr (void)
-{
-
-}
-
-void timer5_isr (void)
-{
-
-}
-
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-//--------------------- External Interrupt Sub Routine ------------------------
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-void ext_int0_isr()
-{
-    
-}
-    
-void ext_int1_isr()
-{
-    
-}
-
-void ext_int2_isr()
-{
-    
-}
-
-void ext_int3_isr()
-{
-
-}
-
-void ext_int4_isr()
-{
-
-}
-
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-//------------------- Output Compare Interrupt Sub Routine --------------------
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-void output_compare0_isr()
-{
-
-}
-
-
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-//--------------------- Uart 1 Interrupt Sub Routine --------------------------
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-void uart_isr (void)
-{
-    #if defined (__18CXX)
-
-    #elif defined(__PIC24F__) || defined(__dsPIC33F__)
-
-        if (U1STAbits.URXDA)                // si donn�e dans buffer RX alors :
-        {
-
-        }
-
-    #endif
-}
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-//----------------------------- Timer Interrupt -------------------------------
+//--------------------------- Interrupts management ---------------------------
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 #if defined (_18F252)
-void __interrupt(high_priority) timer2_irq()
-#elif defined (_18F26K42) || defined (_18F57Q43)
-void __interrupt(irq(TMR2),high_priority) timer2_irq(void)
+void __interrupt(high_priority) interrupt()
+{
+    /*******************************************************/
+    /* Timer 2 interrupt */
+    /*******************************************************/
+    if (PIR1bits.TMR2IF && PIE1bits.TMR2IE){
+        PIR1bits.TMR2IF = 0;     /* clear interrupt flag */
+
+        static u16 CntTmrIncSec = 0;
+
+        CntTmrIncSec++;
+
+        if (CntTmrIncSec == 499){
+            LED_SEC = !LED_SEC;
+        }
+        else if (CntTmrIncSec == 999)
+        {
+            time_has_changed_timer = TRUE;
+            LED_SEC = !LED_SEC;
+            CntTmrIncSec = 0;
+        }
+    }
+
+    /*******************************************************/
+    /* UART interrupt */
+    /*******************************************************/
+    else if (PIR1bits.RCIF && PIE1bits.RCIE){
+        PIR1bits.RCIF = 0;     /* clear interrupt flag */
+        u8 uart_rx_data = RCREG;
+        // command_line_interpreter(UART_ID_1, uart_rx_data);
+    }
+    
+}
 #endif
+
+#if defined (_18F26K42) || defined (_18F57Q43)
+void __interrupt(irq(TMR2),high_priority) timer2_irq(void)
+
 {
     static u16 CntTmrIncSec = 0;
 
@@ -132,3 +82,4 @@ void __interrupt(irq(TMR2),high_priority) timer2_irq(void)
         CntTmrIncSec = 0;
     }
 }
+#endif
